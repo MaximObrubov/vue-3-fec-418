23c45819bc3142fec2b7c42bf59ba515ee62f33e
Maxim Obrubov <m.obrubov@yandex.ru>
2021-07-06 15:28:21 +0400

    FEC-418: added vue

--- index.ts
+++ index.ts
@@ -1,8 +1,26 @@
-import * as express from 'express';
-// rest of the code remains same
+const express = require('express');
+const Vue = require('vue')
 const app = express();
 const PORT = 8000;
+
+
+const Counter = {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++
+    }, 1000)
+  }
+}
+
+Vue.createApp(Counter).mount('#counter')
+
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
